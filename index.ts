849d9fa171b2e804078d6be5e47be81a4898cfb6
Park Young Jun <june0313@gmail.com>
2020-12-31 15:36:37 +0900

    sample of typescript

--- index.ts
+++ index.ts
@@ -1,1 +1,11 @@
-console.log("hello");
+const name = "Wayne",
+  age = 31,
+  gender = "male";
+
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age},  you are a ${gender}`);
+};
+
+sayHi(name, age);
+
+export {};
