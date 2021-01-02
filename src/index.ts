1d64d269f4a57e1d60a1f96374bf6b2fbf1e94d8
Park Young Jun <june0313@gmail.com>
2021-01-02 15:34:14 +0900

    change interface to class

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,33 @@
-const sayHi = (name: string, age: number, gender: string): string => {
-  return `Hello ${name}, you are ${age}, you are a ${gender}!!~~~`;
+class Human {
+  private name: string;
+  private age: number;
+  private gender: string;
+
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+
+  getAge(): number {
+    return this.age;
+  }
+
+  getName(): string {
+    return this.name;
+  }
+
+  getGender(): string {
+    return this.gender;
+  }
+}
+
+const lynn = new Human("lynn", 12, "female");
+
+const sayHi = (person: Human): string => {
+  return `Hello ${person.getName()}, you are ${person.getAge()}, you are a ${person.getGender()}!!~~~`;
 };
 
-console.log(sayHi("nicolas", 333, "male"));
+console.log(sayHi(lynn));
 
 export {};
