c53db8f70590aa89b031e769281189baf2960387
Park Young Jun <june0313@gmail.com>
2021-01-09 11:43:45 +0900

    creating a block #3

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 import * as CryptoJs from "crypto-js";
+import { createNew } from "typescript";
 
 class Block {
   public index: number;
@@ -37,5 +38,31 @@ let blockchain: [Block] = [genesisBlock];
 const getBlockchain = (): Block[] => blockchain;
 const getLatestBlock = (): Block => blockchain[blockchain.length - 1];
 const getNewTimeStamp = (): number => Math.round(new Date().getTime() / 1000);
+
+const createNewBlock = (data: string): Block => {
+  const previousBlock: Block = getLatestBlock();
+  const newIndex: number = previousBlock.index + 1;
+  const newTimeStamp: number = getNewTimeStamp();
+  const newHash: string = Block.calculateBlockHash(
+    newIndex,
+    previousBlock.hash,
+    newTimeStamp,
+    data
+  );
+  const newBlock: Block = new Block(
+    newIndex,
+    newHash,
+    previousBlock.hash,
+    data,
+    newTimeStamp
+  );
+
+  getBlockchain().push(newBlock);
+
+  return newBlock;
+};
+
+console.log(createNewBlock("Hello"), createNewBlock("byebye"));
+console.log(getBlockchain());
 
 export {};
